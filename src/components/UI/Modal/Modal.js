bf271c67a8b2147b50b4bfda2cdd6f73bb748f8b
Fahd <jibreelfahd@gmail.com>
2024-06-11 14:28:03 +0100

    Rendered Modal using Portals

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,18 +1,34 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import Card from "../Card/Card";
 import Cart from "../../Cart/Cart";
 
 import styles from "./Modal.module.css";
 
+// desc: backdrop overlay component for cartmodal
+const Backdrop = () => {
+  return <div className={styles.overlay} />;
+};
 
+const CartModal = () => {
+  return (
+    <Card className={styles.modal}>
+      <Cart />
+    </Card>
+  );
+};
 const Modal = () => {
   return (
     <>
-      <div className={styles.overlay} />
-      <Card className={styles.modal}>
-        <Cart />
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop />,
+        document.querySelector(".backdrop-overlay")
+      )}
+      {ReactDOM.createPortal(
+        <CartModal />,
+        document.querySelector(".modal-overlay")
+      )}
     </>
   );
 };
